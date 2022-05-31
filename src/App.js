08ffc35a09d0624d28f4953d7299346c4e9ecea7
walterunderwood <walonsounderwood@gmail.com>
2022-05-30 18:59:25 -0700

    Creating Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar'
 import AboutMe from './components/AboutMe';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <AboutMe />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const styles = {
+
+};
+
+function Footer() {
+    return (
+        <footer className="container mb-10">
+            <ul className="list-unstyled d-flex mt-5 mb-4">
+                <li className="me-7"><a className="text-body text-decoration-none" href="https://github.com/Wau00">GitHub</a></li>
+                <li className="me-7"><a className="text-body text-decoration-none" href="tel:469-662-2739">Phone</a></li>
+                <li className="me-7"><a className="text-body text-decoration-none"
+                    href="https://www.linkedin.com/in/walter-alonso-underwood-291524234/">LinkedIn</a></li>
+                <li className="me-7"><a className="text-body text-decoration-none"
+                    href="https://www.instagram.com/underwooh/">Instagram</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
